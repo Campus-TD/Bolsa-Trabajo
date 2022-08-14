340d2caeaabb92582731f5b3ec06fbaa4f9bc391
Josephglz <joseph220199@gmail.com>
2022-08-14 14:06:51 -0500

    bootstrap added

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -4,9 +4,15 @@
             {{ __('Formulario') }}
         </h2>
     </x-slot>
-
     <!-- FORM -->
-   
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="container-fluid">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
     <!-- END FORM -->
-
 </x-app-layout>
