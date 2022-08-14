d8222a0d1fe8cd879e2c3be04a293c28c43a0e6b
Josephglz <joseph220199@gmail.com>
2022-08-14 12:09:55 -0500

    route form added

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,7 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+    Route::get('/form', function () {
+        return view('form');
+    })->name('form');
 });
