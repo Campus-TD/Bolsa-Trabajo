6a1960d97ea38a24bc6032f4cf907e2d9affb253
Josephglz <joseph220199@gmail.com>
2022-08-15 20:03:56 -0500

    autentificacion de dos pasos

--- resources/views/profile/two-factor-authentication-form.blade.php
+++ resources/views/profile/two-factor-authentication-form.blade.php
@@ -1,28 +1,28 @@
 <x-jet-action-section>
     <x-slot name="title">
-        {{ __('Autenticación de dos factores') }}
+        {{ __('Autenticación de dos pasos') }}
     </x-slot>
 
     <x-slot name="description">
-        {{ __('Añada seguridad adicional, activando la Autenticación de dos factores.') }}
+        {{ __('Añada seguridad adicional, activando la Autenticación de dos pasos.') }}
     </x-slot>
 
     <x-slot name="content">
         <h3 class="text-lg font-medium text-gray-900">
             @if ($this->enabled)
                 @if ($showingConfirmation)
-                    {{ __('Finalizando la activación de Autenticación de dos factores.') }}
+                    {{ __('Finalizando la activación de Autenticación de dos pasos.') }}
                 @else
-                    {{ __('Tienes activada la Autenticación de dos factores.') }}
+                    {{ __('Tienes activada la Autenticación de dos pasos.') }}
                 @endif
             @else
-                {{ __('No tiene activada la Autenticación de dos factores.') }}
+                {{ __('No tiene activada la Autenticación de dos pasos.') }}
             @endif
         </h3>
 
         <div class="mt-3 max-w-xl text-sm text-gray-600">
             <p>
-                {{ __('Cuando la autenticación de dos factores está habilitada, se le solicitará un token seguro y aleatorio durante la autenticación. Puede recuperar este token de la aplicación Google Authenticator de su teléfono.') }}
+                {{ __('Cuando la autenticación de dos pasos está habilitada, se le solicitará un token seguro y aleatorio durante la autenticación. Puede recuperar este token de la aplicación Google Authenticator de su teléfono.') }}
             </p>
         </div>
 
@@ -31,9 +31,9 @@
                 <div class="mt-4 max-w-xl text-sm text-gray-600">
                     <p class="font-semibold">
                         @if ($showingConfirmation)
-                            {{ __('Para terminar de habilitar la autenticación de dos factores, escanee el siguiente código QR usando la aplicación de autenticación de su teléfono o ingrese la clave de configuración y proporcione el código OTP generado.') }}
+                            {{ __('Para terminar de habilitar la autenticación de dos pasos, escanee el siguiente código QR usando la aplicación de autenticación de su teléfono o ingrese la clave de configuración y proporcione el código OTP generado.') }}
                         @else
-                            {{ __('La autenticación de dos factores ahora está habilitada. Escanee el siguiente código QR usando la aplicación de autenticación de su teléfono o ingrese la clave de configuración.') }}
+                            {{ __('La autenticación de dos pasos ahora está habilitada. Escanee el siguiente código QR usando la aplicación de autenticación de su teléfono o ingrese la clave de configuración.') }}
                         @endif
                     </p>
                 </div>
@@ -64,7 +64,7 @@
             @if ($showingRecoveryCodes)
                 <div class="mt-4 max-w-xl text-sm text-gray-600">
                     <p class="font-semibold">
-                        {{ __('Guarde estos códigos de recuperación en un administrador de contraseñas seguro. Se pueden usar para recuperar el acceso a su cuenta si se pierde su dispositivo de autenticación de dos factores.') }}
+                        {{ __('Guarde estos códigos de recuperación en un administrador de contraseñas seguro. Se pueden usar para recuperar el acceso a su cuenta si se pierde su dispositivo de autenticación de dos pasos.') }}
                     </p>
                 </div>
 
