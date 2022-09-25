f8c015984a3dae8faea845fc3f02600dd1d94092
Josephglz <joseph220199@gmail.com>
2022-09-25 14:42:52 -0500

    Error auth messages translate.

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Las credenciales de acceso no coinciden.',
+    'password' => 'La contraseña es incorrecta.',
+    'throttle' => 'Demasiados intentos. Porfavor intenta en :seconds segundos.',
 
 ];
