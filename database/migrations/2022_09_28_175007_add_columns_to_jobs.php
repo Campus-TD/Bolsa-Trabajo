ea8152562eb72baa7a00c0f062e9306f7ba04145
Josephglz <joseph220199@gmail.com>
2022-09-28 17:39:21 -0500

    new columns added to job.

---
+++ database/migrations/2022_09_28_175007_add_columns_to_jobs.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('jobs', function (Blueprint $table) {
+            $table->string('business_turn')->nullable();
+            $table->float('salary')->nullable();
+            $table->integer('english')->default(false);
+            $table->integer('approved')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('jobs', function (Blueprint $table) {
+            //
+        });
+    }
+};
